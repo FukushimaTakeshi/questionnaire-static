47d627c912a5c24b4c6f50a9d5d6adf59dbd2dfe
FukushimaTakeshi <whippet.818@gmail.com>
2019-02-25 22:10:19 +0900

    Install vee-validate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+// import 'babel-polyfill'
 import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 Vue.use(Buefy)
+
+import ja from 'vee-validate/dist/locale/ja.js'
+import VeeValidate, { Validator } from 'vee-validate';
+Vue.use(VeeValidate, { locale: ja })
+Validator.localize('ja', ja)
 
 import App from './App.vue'
 

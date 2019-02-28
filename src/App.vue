311598cd9a5ce7337ae20e77c0e133d9d84b81ad
FukushimaTakeshi <whippet.818@gmail.com>
2019-02-28 22:58:06 +0900

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,245 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <section class="hero is-info is-medium is-bold">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">アンケートだよ</h1>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <section class="articles">
+        <div class="column is-8 is-offset-2">
+          <div class="card article">
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content has-text-centered">
+                  <p class="title article-title">Introducing a new feature for paid subscribers</p>
+                  <div class="tags has-addons level-item">
+                    <span class="tag is-rounded is-info">@skeetskeet</span>
+                    <span class="tag is-rounded">May 10, 2018</span>
+                  </div>
+                </div>
+              </div>
+              <div class="content article-body">
+                <p>hogehoge</p>
+                
+                <h3 class="has-text-left">How to properly center tags in bulma?</h3>
+                <p> Proper centering of tags in bulma is done with class: </p>hogehoge.<p></p>
+                <div class="field">
+                  <label class="label">名前</label>
+                  <div class="control">
+                    <input class="input" type="text" name="name" v-model="answer.name">
+                  </div>
+                  <p class="help is-danger" v-show="!validation.name">必須項目だよ</p>
+                </div>
+
+                <div class="field">
+                  <label class="label">Q.1</label>
+                  <p>扱った内容のレベルは?</p>
+                  <p></p>
+                  
+                  <div class="control">
+                    
+                    <div class="field" v-for="(level, index) in levels" :key="index">
+                      <label class="radio">
+                        <input type="radio" name="level" :value="level" v-model="answer.level">
+                        {{ level }}
+                      </label>
+                    </div>
+                    <p class="help is-danger" v-show="!validation.level">選択してね</p>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">Q.2</label>
+                  <p>全員参加型の輪読会でしたが、自ら積極的に発言できましたか?</p>
+                  <p></p>
+                  <div class="control">
+                    <div class="field" v-for="(activeLevel, index) in activeLevels" :key="index">
+                      <label class="radio">
+                        <input type="radio" name="active" :value="activeLevel" v-model="answer.activeLevel">
+                        {{ activeLevel }}
+                      </label>
+                    </div>
+                    <p class="help is-danger" v-show="!validation.activeLevel">選択してね</p>
+                  </div>
+                </div>
+
+                <div class="field" v-if="answer.activeLevel === activeLevels[2] || answer.activeLevel === activeLevels[3]">
+                  <label class="label">Q.2-1</label>
+                  <p>その理由は?</p>
+                  <p></p>
+                  <div class="control">
+                    <div class="field" v-for="(reason, index) in reasons" :key="index">
+                      <label class="radio">
+                        <input type="radio" name="reason" :value="reason" v-model="answer.reason">
+                        {{ reason }}
+                      </label>
+                    </div>
+                    <p class="help is-danger" v-show="!validation.reason">選択してね</p>
+                    <div class="field">
+                      <input class="input" type="text" name="reasonComment" placeholder="それは、具体的にどんなこと？" 
+                        v-model="answer.reasonComment"
+                      >
+                    </div>
+                  </div>
+                  <p class="help is-danger" v-show="!validation.reasonComment">書いてくだちい (´；ω；｀)</p>
+                </div>
+
+                <div class="field">
+                  <label class="label">Q.3</label>
+                  <p>次に輪読会や勉強会に参加するとしたら、どんなテーマがいい？ (複数選択可)</p>
+                  <p class="help is-danger" v-show="!validation.nextTheme">選択してね</p>
+                  <div class="control">
+                    <div class="field" v-for="(nextTheme, index) in nextThemes" :key="index">
+                      <b-tooltip :label="nextTheme.tooltip" position="is-right">
+                        <label class="checkbox">
+                          <input type="checkbox" :id="index" :value="nextTheme.comment" v-model="answer.nextThemes">
+                          {{ nextTheme.comment }}
+                        </label>
+                        <p class="control" v-if="nextTheme.added">
+                          <button class="button is-light" @click="removeTheme(nextTheme)">消す</button>
+                        </p>
+                      </b-tooltip>
+                    </div>
+
+                    <div class="field has-addons">
+                      <p class="control is-expanded">
+                        <input class="input" type="text" placeholder="他にやりたいテーマがあれば書いてね" v-model="themeText">
+                      </p>
+                      <p class="control">
+                        <button class="button is-light" @click="addTheme()">追加する</button>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="field">
+                  <label class="label">Q.4</label>
+                  <p>その他、感想や要望など、言いたいことなんでも</p>
+                  <div class="control">
+                    <textarea class="textarea" placeholder="適当に書いてね^^" v-model="answer.freeComment"></textarea>
+                  </div>
+                  <p class="help is-danger" v-show="!validation.freeComment">多いかな</p>
+                </div>
+
+                <div class="field">
+                  <div class="control">
+                    <button @click="registerAnswer()" class="button is-block is-info is-fullwidth">送信する</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </section>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      levels: [
+        '既に知っていることだった (簡単だった)',
+        'ちょうどよかった',
+        'ついていくのがやっと',
+        '難しくて理解できなかった'
+      ],
+      activeLevels: [
+        'できた！',
+        'まぁまぁできたかな^^',
+        'あまりできなかったよ',
+        'ほとんどできなかったorz'
+      ],
+      reasons: [
+        '技術的なこと',
+        '技術以外なこと'
+      ],
+      nextThemes: [
+        { comment: 'プログラミング言語の基本的なところ', tooltip: 'ex.基本文法やクラスとは? など入門書に書いてあるような基礎的な内容などです' },
+        { comment: 'プログラミング言語を深く掘り下げ、イディオムや問題解決のための言語独特の手法', tooltip: 'ex.書籍でいう「Effective〜」に代表される言語学習の第2フェーズ的位置づけの内容' },
+        { comment: '良いコードを書くための方法論', tooltip: 'リーダブルコードに近いものを別のテーマで' },
+        { comment: '設計に関するテーマ1 (デザインパターン、オブジェクト指向のこころなど)', tooltip: '' },
+        { comment: '設計に関するテーマ2 (DDD、Clean Architectureのようなもの)', tooltip: '' },
+        { comment: '正直、何したらいいかわからない', tooltip: '(´；ω；｀)' }
+      ],
+      themeText: '',
+      answer: {
+        name: '',
+        level: '',
+        activeLevel: '',
+        reason: '',
+        reasonComment: '',
+        nextThemes: [],
+        freeComment: ''
+      }
+    }
+  },
+  computed: {
+    validation() {
+      const answer = this.answer
+      const optional = (answer.activeLevel === this.activeLevels[2] || answer.activeLevel === this.activeLevels[3])
+
+      return {
+        name: !!answer.name.trim(),
+        level: !!answer.level.trim(),
+        activeLevel: !!answer.activeLevel.trim(),
+        reason: (optional ? !!answer.reason.trim() : true),
+        reasonComment: (optional ? !!answer.reasonComment.trim() : true),
+        nextTheme: !(answer.nextThemes.length === 0),
+        freeComment: (answer.freeComment.length < 1000)
+      }
+    },
+    isValid() {
+      const validation = this.validation
+      return Object.keys(validation).every((key) => {
+        return validation[key]
+      })
+    }
+  },
+  methods: {
+    async registerAnswer() {
+      if (!this.isValid) { 
+        this.danger()
+        return
+      }
+      const res = await axios.post(`/api/hoges`, { answer: this.answer })
+      if (res.status !== 200) { return }
+    },
+    addTheme() {
+      const theme = this.themeText.trim()
+      if (!theme) { return }
+      this.nextThemes.push({
+        comment: theme,
+        tooltip: '',
+        added: true
+      })
+			this.themeText = ''
+    },
+    removeTheme(item) {
+      const index = this.nextThemes.indexOf(item)
+      this.nextThemes.splice(index, 1)
+    },
+    danger() {
+      this.$toast.open({
+        duration: 3000,
+        message: `ちゃんと入力できてないよ`,
+        position: 'is-bottom',
+        type: 'is-danger'
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.articles {
+  margin: 5rem 0;
+  margin-top: -100px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 Vue.use(Buefy)
 
-import ja from 'vee-validate/dist/locale/ja.js'
-import VeeValidate, { Validator } from 'vee-validate';
-Vue.use(VeeValidate, { locale: ja })
-Validator.localize('ja', ja)
-
 import App from './App.vue'
 
 Vue.config.productionTip = false

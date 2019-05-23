b49c5b6774adf0c193b274cfd078341ca3c95a13
FukushimaTakeshi <whippet.818@gmail.com>
2019-05-23 21:37:17 +0900

    Install asw-amplify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // import 'babel-polyfill'
 import Vue from 'vue'
+import App from './App2.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 Vue.use(Buefy)
 
-import App from './App.vue'
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
 
 Vue.config.productionTip = false
 
